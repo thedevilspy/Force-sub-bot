af969ce0072ef438f7612f535d770d1439dd6d5c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-04-11 16:23:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
+
+    CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", "")
